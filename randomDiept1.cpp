005c1b1727dd95f46302f2a8f38c060e4ead53a3
jadeeng <51209243+jadeeng@users.noreply.github.com>
2020-03-06 13:00:01 -0800

    Create randomDiept1.cpp

---
+++ randomDiept1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include<ctime>
+using namespace std;
+using std::time;
+
+int main()
+{
+int randomDie;
+srand(time(0));
+for (int i=1; i<=2; i++){
+    cout<<"The value of die " <<i<<" is "<<1+(rand()%6)<<".\n";
+}
+
+    return 0;
+}
